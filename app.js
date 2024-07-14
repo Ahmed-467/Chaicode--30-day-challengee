f9de3839d8d2fb9679307042425b41baf9b679bc
Ahmed-467 <hplaptop03421@gmail.com>
2024-07-14 12:18:53 +0500

    task

---
+++ app.js
@@ -0,0 +1,61 @@
+console.log("javascript 30 days challenge");
+
+//Day 1: variable and Data types
+
+//Task 1: Declare a variable using var, assign it a number, and the value to the console,
+
+
+//task 1: ANS
+var num = 5
+console.log(num);
+
+
+//task 2: Declare a variable using let, assign it a string, and the value to the console,
+
+//Task 2: ANS
+let mystr = 'Ahmed'
+console.log('mystr');
+
+//Task 3 Declare a variable using const, assign it a boolean value, and log the value to the console.
+
+//Task: 3 AnS.
+const myboolean = true;
+console.log(myboolean);
+
+// task 4: Create varibales of different data type (number,string, boolean, object, array) and log each variables type using the (typeof) opertors
+
+//Task 4 Ans:
+
+const mynam = 'Ahmed Nadeem';
+const myage = '19';
+const islogedIn = true;
+const myarray = ['Ahmed','Nadeem',"19"];
+const myobject = {
+    name: 'Ahmed Nadeem Siddiuqe',
+    email: 'hplaptop03421@gmail.com',
+}
+console.log(typeof mynam);
+console.log(typeof myage);
+console.log(typeof islogedIn);
+console.log(typeof myarray);
+console.log(typeof myobject);
+
+
+//Task 5: Declare a variable using (let), assign it an initial value, reassign a new value, and log both values to the console.
+
+
+//Task 5 Ans:
+let bankbalance = 10000
+console.log("BeforeTransaction", 10000);
+bankbalance = 20000
+console.log('AfterTransaction',20000);
+
+//Task 6: Try reassigning a variable declared with (const) and observe the error.
+
+//Task 6: Ans
+let userid = 'Ahmed'
+console.log(userid);
+userid = 'Ahmed';
+
+
+// Day 1: Task completed ///
