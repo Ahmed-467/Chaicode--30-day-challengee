36e23a82668f2bde20d80801e9ad8cd7c4c0d5d1
Ahmed-467 <hplaptop03421@gmail.com>
2024-07-17 12:18:47 +0500

    task

--- day 4 loops/app.js
+++ day 4 loops/app.js
@@ -8,3 +8,12 @@ for(let i =1; i <= 10; i++){
     console.log(i);
 }
 
+//. Task 2: write a program to print the multiplication table 5 using a for loop.
+// Multiplication table for 5
+const number = 5;
+
+// Using a for loop to print the multiplication table
+for (let i = 1; i <= 10; i++) {
+  console.log(`${number} x ${i} = ${number * i}`);
+}
+
