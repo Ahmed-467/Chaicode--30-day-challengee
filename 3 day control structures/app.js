8fc4f5a00df9fff2391ea314e04184d0f79d578c
Ahmed-467 <hplaptop03421@gmail.com>
2024-07-17 11:35:13 +0500

    task

--- 3 day control structures/app.js
+++ 3 day control structures/app.js
@@ -82,3 +82,42 @@ getDayName(4); // Output: Wednesday
 getDayName(7); // Output: Saturday
 getDayName(8); // Output: Invalid day number. Please enter a number between 1 and 7.
 
+
+
+// • Task 5: Write a program that uses a switch case to assign a grade ('A', 'B', 'C', 'D', 'F') based on a score and log the grade to the console.
+function getGrade(score) {
+    let grade;
+    switch (true) {
+     case (score >= 90 && score <= 100):
+            grade = 'A';
+            break;
+       case (score >= 80 && score < 90):
+            grade = 'B';
+            break;
+      case (score >= 70 && score < 80):
+            grade = 'C';
+            break;
+      case (score >= 60 && score < 70):
+            grade = 'D';
+            break;
+        case (score >= 0 && score < 60):
+            grade = 'F';
+            break;
+        default:
+            grade = 'your are sucessfully passed';
+    }
+    console.log(`Score: ${score}, Grade: ${grade}`);
+}
+
+// Test the function with different inputs
+getGrade(95); // Output: Score: 95, Grade: A
+getGrade(85); // Output: Score: 85, Grade: B
+getGrade(75); // Output: Score: 75, Grade: C
+getGrade(65); // Output: Score: 65, Grade: D
+getGrade(55); // Output: Score: 55, Grade: F
+getGrade(110); // Output: Invalid score. Please enter a score between 0 and 100.
+
+
+
+
+
