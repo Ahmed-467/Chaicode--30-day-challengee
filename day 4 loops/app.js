c15d5e0badf899e454725a7ad5c7b4a85621b17a
Ahmed-467 <hplaptop03421@gmail.com>
2024-07-17 12:13:12 +0500

    task

---
+++ day 4 loops/app.js
@@ -0,0 +1,10 @@
+/// Task 4: loops
+
+//------------ Task-------------Activites://
+
+//.write a program to print number from 1 to 10 using in loop
+
+for(let i =1; i <= 10; i++){
+    console.log(i);
+}
+
