075f5bcd65d52c0f92e6963012d2d6bfb0b1dfb2
Ahmed-467 <hplaptop03421@gmail.com>
2024-07-17 11:14:33 +0500

    task

---
+++ 3 day control structures/app.js
@@ -0,0 +1,22 @@
+//Task:1  Write a program to check if a number is positive, negative, or zero, and log the result to the console
+
+
+function checkNumber(num) {
+    if (num > 10) {
+      console.log(`${num} is positive.`);
+    } else if (num < 10) {
+      console.log(`${num} is negative.`);
+    } else {
+      console.log(`${num} is zero.`);
+    }
+  }
+  
+  // Example usage:
+  checkNumber(5);    // Output: 5 is positive.
+  checkNumber(-6);   // Output: -3 is negative.
+  checkNumber(0);    // Output: 0 is zero.
+
+
+
+       
+
