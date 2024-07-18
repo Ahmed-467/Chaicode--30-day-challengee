b48a0e3c0d4d2d1de213ce8adbe112348569b370
Ahmed-467 <hplaptop03421@gmail.com>
2024-07-18 10:58:37 +0500

    task

---
+++ day 5 funcation/app.js
@@ -0,0 +1,20 @@
+// Day 5: Functions
+
+// Tasks/Activities:
+
+//--------- Activity 1: Function Declaration------------
+
+//.Task 1 write a funcation to check if a number is even or odd and the log the result to the console.
+function checkEvenOdd(number) {
+    if (number % 2 === 0) {
+        console.log(number + " is even.");
+    } else {
+        console.log(number + " is odd.");
+    }
+}
+
+// Example usage:
+checkEvenOdd(10); // 10 is even.
+checkEvenOdd(7);  // 7 is odd.
+
+
