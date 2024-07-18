eef8b88577c16a130d28867184b833c3904c4458
Ahmed-467 <hplaptop03421@gmail.com>
2024-07-18 10:48:15 +0500

    task

--- day 4 loops/app.js
+++ day 4 loops/app.js
@@ -73,6 +73,17 @@ for (let i = 1; i <= rows; i++) {
     console.log(pattern);
 }
 
+//---------------- Activity 5: Loop Control Statements-----------------
+
+// • Task 8: Write a program to print numbers from 1 to 10, but skip the number 5 using the continue statement.
+
+for(let i = 1; i <= 10; i++){
+   if(i === 5){
+    continue
+   }
+   console.log(i);
+}
+
 
 
 
