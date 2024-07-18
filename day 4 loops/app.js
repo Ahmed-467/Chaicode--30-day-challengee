669c8aaf9b1011c3268c0c3bfee373618ccbe682
Ahmed-467 <hplaptop03421@gmail.com>
2024-07-18 10:51:27 +0500

    task

--- day 4 loops/app.js
+++ day 4 loops/app.js
@@ -84,6 +84,17 @@ for(let i = 1; i <= 10; i++){
    console.log(i);
 }
 
+// • Task 9: Write a program to print numbers from 1 to 10, but stop the loop when the number is 7 using the break statement.
+
+for(let i = 1; i <= 10; i++){
+  if(i === 7){
+    break
+  }
+  console.log(i);
+}
+
+//================= Complete=================// 
+
 
 
 
