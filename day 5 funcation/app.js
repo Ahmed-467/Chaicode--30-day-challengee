dcd856d6224e30d2fc204a0fe6c50230eb2c9f0a
Ahmed-467 <hplaptop03421@gmail.com>
2024-07-18 11:08:51 +0500

    task

--- day 5 funcation/app.js
+++ day 5 funcation/app.js
@@ -5,16 +5,32 @@
 //--------- Activity 1: Function Declaration------------
 
 //.Task 1 write a funcation to check if a number is even or odd and the log the result to the console.
-function checkEvenOdd(number) {
-    if (number % 2 === 0) {
-        console.log(number + " is even.");
-    } else {
-        console.log(number + " is odd.");
-    }
+// function checkEvenOdd(number) {
+//     if (number % 2 === 0) {
+//         console.log(number + " is even.");
+//     } else {
+//         console.log(number + " is odd.");
+//     }
+// }
+
+// // Example usage:
+// checkEvenOdd(10); // 10 is even.
+// checkEvenOdd(7);  // 7 is odd.
+
+
+//. Task 2: Write a funcation to calculate the square of number and reture the result.
+
+function calculateSquare (number){
+    return number * number;
 }
 
-// Example usage:
-checkEvenOdd(10); // 10 is even.
-checkEvenOdd(7);  // 7 is odd.
+//----------Example using---------------//
+let result1 = calculateSquare(4);
+let result2 = calculateSquare(8);
+
+console.log(result1);
+console.log(result2);
+ 
+
 
 
